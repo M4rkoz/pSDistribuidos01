bbc25518e3505c3fed142554ad0e47d024dc2415
m_acuna <marcoantonioraiacuna@gmail.com>
2024-03-12 10:58:33 -0400

    Primer commit...

---
+++ pSDistribuidos01/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using pTopicos02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
---
+++ pSDistribuidos01/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace pTopicos02
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/api/calculator/sumar", async context =>
+                {
+                    var numero1 = int.Parse(context.Request.Query["numero1"]);
+                    var numero2 = int.Parse(context.Request.Query["numero2"]);
+                    var resultado = numero1 + numero2;
+
+                    await context.Response.WriteAsJsonAsync(new { resultado });
+                });
+            });
+        }
+    }
+}
